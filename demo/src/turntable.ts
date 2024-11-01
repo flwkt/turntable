ad0e807219bfe88ab10898c7472a3d5a7a308d4d
Stephan Schober <teh.fonsi@gmail.com>
2024-11-01 20:33:03 +0100

    chore: add types

--- demo/src/turntable.ts
+++ demo/src/turntable.ts
@@ -1,20 +1,18 @@
-/// <reference lib="dom" />
-
-const updateActiveImage = (container: HTMLElement, index: number) => {
-  const images = container.querySelectorAll('li');
-  images.forEach((img) => img.classList.remove('active'));
+const updateActiveImage = (container: HTMLElement, index: number): void => {
+  const images: NodeListOf<HTMLLIElement> = container.querySelectorAll('li');
+  images.forEach((img: HTMLLIElement) => img.classList.remove('active'));
   images[index]?.classList.add('active');
 };
 
-const initTurntable = () => {
-  const containers = document.querySelectorAll<HTMLDivElement>(
-    '[data-turntable-file]'
-  );
+const initTurntable = (): void => {
+  const containers: NodeListOf<HTMLDivElement> =
+    document.querySelectorAll<HTMLDivElement>('[data-turntable-file]');
   if (!containers.length) return;
 
   // Add required CSS
-  document.head.appendChild(
-    Object.assign(document.createElement('style'), {
+  const style: HTMLStyleElement = Object.assign(
+    document.createElement('style'),
+    {
       textContent: `
       .turntable { margin: 0; }
       .turntable ul { padding: 0; margin: 0; }
@@ -22,18 +20,30 @@ const initTurntable = () => {
       .turntable ul li img { width: 100%; }
       .turntable ul li.active { display: block; }
     `,
-    })
+    }
   );
+  document.head.appendChild(style);
 
-  containers.forEach((container) => {
-    const file = container.getAttribute('data-turntable-file');
+  containers.forEach((container: HTMLDivElement) => {
+    const file: string | null = container.getAttribute('data-turntable-file');
     if (!file) return;
 
-    const settingCount = container.getAttribute('data-turntable-count');
-    const settingStep = container.getAttribute('data-turntable-step');
+    const settingCount: string | null = container.getAttribute(
+      'data-turntable-count'
+    );
+    const settingStep: string | null = container.getAttribute(
+      'data-turntable-step'
+    );
 
-    const imageCount = settingCount || 10; // default is 10 images
-    const angleStep = settingStep || 360 / imageCount;
+    let imageCount: number = 10;
+    let angleStep: number = 360 / imageCount;
+    if (settingCount && !settingStep) {
+      imageCount = parseInt(settingCount);
+      angleStep = 360 / imageCount;
+    } else if (settingStep && !settingCount) {
+      angleStep = parseInt(settingStep);
+      imageCount = 360 / angleStep;
+    }
 
     // Setup container
     container.classList.add('turntable');
@@ -42,17 +52,21 @@ const initTurntable = () => {
     }
 
     // Create images
-    const ul = container.appendChild(document.createElement('ul'));
-    for (let i = 0; i < imageCount; i++) {
-      const li = ul.appendChild(document.createElement('li'));
+    const ul: HTMLUListElement = container.appendChild(
+      document.createElement('ul')
+    );
+    for (let i: number = 0; i < imageCount; i++) {
+      const li: HTMLLIElement = ul.appendChild(document.createElement('li'));
       if (i === Math.floor(imageCount / 2)) li.classList.add('active');
 
-      const angle = -180 + i * angleStep;
-      const url = `https://www.flowkit.app/s/demo/r/rh:${angle},rv:15,s:256/u/${encodeURIComponent(
+      const angle: number = -180 + i * angleStep;
+      const url: string = `https://www.flowkit.app/s/demo/r/rh:${angle},rv:15,s:256/u/${encodeURIComponent(
         file
       )}`;
 
-      const img = li.appendChild(document.createElement('img'));
+      const img: HTMLImageElement = li.appendChild(
+        document.createElement('img')
+      );
       const lazy = container.getAttribute('data-turntable-lazy');
       if (lazy) {
         img.loading = 'lazy';
@@ -61,26 +75,26 @@ const initTurntable = () => {
     }
 
     // Add interaction handlers
-    const handleInteraction = (x: number) => {
-      const rect = container.getBoundingClientRect();
-      const percent = (x - rect.left) / rect.width;
-      const index = Math.floor((1 - percent) * imageCount);
+    const handleInteraction = (x: number): void => {
+      const rect: DOMRect = container.getBoundingClientRect();
+      const percent: number = (x - rect.left) / rect.width;
+      const index: number = Math.floor((1 - percent) * imageCount);
       if (index >= 0 && index < imageCount) {
         updateActiveImage(container, index);
       }
     };
 
     // Mouse/touch events
-    const isMobile =
+    const isMobile: boolean =
       /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
         navigator.userAgent
       );
     if (isMobile) {
-      container.addEventListener('touchmove', (e) =>
+      container.addEventListener('touchmove', (e: TouchEvent) =>
         handleInteraction(e.touches[0].clientX)
       );
     } else {
-      container.addEventListener('mousemove', (e) =>
+      container.addEventListener('mousemove', (e: MouseEvent) =>
         handleInteraction(e.clientX)
       );
     }
@@ -88,16 +102,16 @@ const initTurntable = () => {
     // Scroll events
     const scroll = container.getAttribute('data-turntable-scroll');
     if (scroll) {
-      window.addEventListener('scroll', () => {
-        const rect = container.getBoundingClientRect();
-        const viewportHeight = window.innerHeight;
+      window.addEventListener('scroll', (): void => {
+        const rect: DOMRect = container.getBoundingClientRect();
+        const viewportHeight: number = window.innerHeight;
 
         if (rect.bottom < 0 || rect.top > viewportHeight) return;
 
-        const progress =
+        const progress: number =
           (viewportHeight - rect.top) / (viewportHeight + rect.height);
-        const degrees = Math.max(0, Math.min(360, progress * 360));
-        const imageIndex =
+        const degrees: number = Math.max(0, Math.min(360, progress * 360));
+        const imageIndex: number =
           Math.floor((degrees / 360) * imageCount) % imageCount;
 
         updateActiveImage(container, imageIndex);
